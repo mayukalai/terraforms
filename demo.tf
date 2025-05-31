ee6a65c086cfce74e78440cc00c5b133ffc4b610
Ubuntu <ubuntu@ip-172-31-9-61.ap-south-1.compute.internal>
2025-05-31 12:48:48 +0000

    Added New TerraForm Configurartion

--- demo.tf
+++ demo.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0-beta2"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
 resource "aws_instance" "this" {
   ami                     = "ami-0e35ddab05955cf57"
   instance_type           = "t2.micro"
